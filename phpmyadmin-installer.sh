60cd0015821f6fab3129f60edb7c3dc352db7d5a
RugaCoder <fruganyumisa@gmail.com>
2019-10-21 13:43:32 +0300

    Created phpmyadmin installer

---
+++ phpmyadmin-installer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "This is the phpmyadmin installation wizard"
+echo "          *"
+echo "        *    *"
+echo "      *         *"
+echo "     *           *"
+echo "     *            *"
+echo "     **************"
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt install phpmyadmin php-gettext php-mbstring
